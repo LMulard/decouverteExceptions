e8145840c45520348461f2b2c25f5b704764c63e
LMulard <mulard.laure@gmail.com>
2018-03-19 16:10:01 +0100

    Ajout d'une exception.

--- src/com/LMulard/program.java
+++ src/com/LMulard/program.java
@@ -1,10 +1,18 @@
 package com.LMulard;
 
+import java.io.File;
+
 public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			// TODO: handle exception
+			erreur.printStackTrace();
+		}
 	}
 
 }
